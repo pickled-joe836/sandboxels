3eb76b0dc930202b45ad736b6924fbb9305a60a7
pickled-joe836 <maxwell.kenney@outlook.com>
2025-02-03 17:43:15 -0600

    Add files via upload

---
+++ pooy.js
@@ -0,0 +1,16 @@
+elements.plord_bomb = {
+	color: "#eb9c4d",
+	behavior: behaviors.POWDER
+[
+	["XX", "XX", "XX",]
+	["XX", "XX", "EX:40>fallout",]
+	["EX:130>fire", "EX:55>plasma", "EX:90>radiation",]
+	category: "weapons",
+	state: "solid",
+	temphigh: "400",
+	stateHigh:"molten_steel",
+	density: "90"
+	reactions: {
+	"uranium": { elem1: "plordium", elem2: plordium"
+	}
+}
